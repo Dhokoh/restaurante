b36fec21819f2fa5a92b025cc50c1a3f9d24717d
Dhokoh <davidhogarcia@gmail.com>
2022-12-02 17:13:11 -0500

    feat: added features to Home page, color on nav, picture and paragraph, modelled jumbotron

--- src/components/pages/GlobalContainer.js
+++ src/components/pages/GlobalContainer.js
@@ -1,6 +1,5 @@
 import Header from '../shared/Header';
 import Footer from '../shared/Footer';
-import Body from '../shared/Body'
 import {Outlet} from 'react-router-dom';
 
 const GlobalContainer = () => {
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,24 @@
-import Body from '../shared/Body'
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 const Home = () => {
     return (
-        <h1>Home</h1>
-    )
+        <Container className='jumbotron'>
+            <h1 className='restaurantBrandHome'>Restaurante</h1>
+            <Row className="homeRow justify-content-md-center">
+                <Col>
+                    <div className='jumbotronPictureContainer'>
+                        <img src='../public/images/LuRouFan.jpg' alt='some dish picture'/>
+                    </div>
+                </Col>
+                <Col>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 export default Home;
