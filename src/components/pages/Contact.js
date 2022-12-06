f6e7a07266845b8ea58eed2695ac2b21755c3c56
Dhokoh <davidhogarcia@gmail.com>
2022-12-06 12:46:25 -0500

    feat: added favicon, attempt to set back end interaction with Firebase

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,4 +9,5 @@ const Contact = () => {
         </Container>
     </>)
 };
+
 export default Contact;
--- src/components/shared/ContactForm.js
+++ src/components/shared/ContactForm.js
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+
+
 const ContactForm = () => {
     return (
         <>
@@ -9,7 +11,7 @@ const ContactForm = () => {
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicText">
                         <Form.Label>Nombre</Form.Label>
-                        <Form.Control className="nameField" type="text" placeholder="¿Cómo te llamas?" />
+                        <Form.Control id="namefield" className="nameField" type="text" placeholder="¿Cómo te llamas?" />
                         <Form.Text className="text-muted">
                             O ¿cómo prefieres ser llamado?
                         </Form.Text>
@@ -17,17 +19,17 @@ const ContactForm = () => {
 
                     <Form.Group>
                         <Form.Label>Tel</Form.Label>
-                        <Form.Control className="nameField" type='number' placeholder="Tel o Cel"></Form.Control>
+                        <Form.Control id="telephonefield" className="nameField" type='number' placeholder="Tel o Cel"></Form.Control>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>E-mail</Form.Label>
-                        <Form.Control className="nameField" type="email" placeholder="email" />
+                        <Form.Control id="emailfield" className="nameField" type="email" placeholder="email" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         <Form.Label>Mensaje</Form.Label>
-                        <Form.Control className='messageField' as="textarea" placeholder="Escribe aquí tu mensaje" />
-                        <Button className="contactButtonStyling" variant="primary" type="submit">
+                        <Form.Control id="msgfield" className='messageField' as="textarea" placeholder="Escribe aquí tu mensaje, o especifica tu reserva" />
+                        <Button id="sendButton" className="contactButtonStyling" variant="primary" type="submit" onClick="">
                         Enviar
                     </Button>
                     </Form.Group>
@@ -39,4 +41,15 @@ const ContactForm = () => {
         </>
     )
 };
+
+const sendData = () => {
+    let newClient = {
+        name: document.getElementById('namefield').value,
+        tel: document.getElementById('telephonefield').value,
+        email: document.getElementById('emailfield').value,
+        msg: document.getElementById('msgfield').value
+    }
+
+}
+
 export default ContactForm;
